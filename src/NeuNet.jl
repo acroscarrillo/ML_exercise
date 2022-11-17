f6d263db4e728dac365bc8b12eae50ab8884a7a4
Alex Cros <a.croscarrillo@gmail.com>
2022-11-17 17:01:38 +0000

    backprop half way done

--- src/NeuNet.jl
+++ src/NeuNet.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 struct NeuNet
     name::String
     layer_arch::Vector{Int}
@@ -5,7 +7,7 @@ struct NeuNet
     biases::Vector{Vector{Float32}}
 end
 
-#to do: initialise weights and biases with the Catalan's algorithm
+#to do: initialise weights and biases with the catalan's algorithm
 function NeuNet(name::String, layer_arch::Vector{Int})
     if length(layer_arch)<2
         error(string("Neural Net must have at least two neuron layers but ",length(layer_arch)," where given."))
@@ -19,18 +21,29 @@ function propagate(net::NeuNet, input::Vector, activation::Function)
     if length(input) != net.layer_arch[1]
         error( string( "Input data of size ",length(input)," is incompatible with input layer of size ",net.layer_arch[1] ) )
     end
-    previous_layer = input
-    for i in 1:length(net.weights)
-        previous_layer = activation.( transpose(net.weights[i])*previous_layer + net.biases[i+1] )
+
+    layer_actv  = [ zeros(net.layer_arch[i]) for i in 1:length(net.layer_arch) ]
+    layer_actv[1] = activation.( input + net.biases[1] )
+    for i in 1:length(net.layer_arch)
+        layer_actv[i+1] = activation.( transpose(net.weights[i])*layer_actv[i] + net.biases[i+1] )
     end
-    return previous_layer
+    return layer_actv
 end
 
-function sigmoid(x::Real)
+function sigmoid(x::Float32)
     return  1.0 / (1.0 + exp(-x))
 end
 
+function back_prop(net::NeuNet, input::Vector, output_e::Vector, activation::Function, learning_rate::Float32)
+    layer_actv = propagate(net,input,activation)
+    grad = qcost(layer_actv[end], output_e)
+    for i in length(net.layer_arch):1
+        grad *= 
+        net.biases[i] = net.biases[i] - learning_rate*(grad)
+    end
+end
 
-function cost(real_output, expected_output)
-    
+#this is technically the derivative of the quadratic cost function
+function qcost(real_output, expected_output)
+    return norm(real_output-expected_output)
 end
