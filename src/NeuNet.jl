af3e1f6a107f2570c74941e87f69c4be88f1f723
Alex Cros <a.croscarrillo@gmail.com>
2022-11-17 12:39:42 +0000

    propagate function works now

--- src/NeuNet.jl
+++ src/NeuNet.jl
@@ -8,28 +8,28 @@ end
 #to do: initialise weights and biases with the Catalan's algorithm
 function NeuNet(name::String, layer_arch::Vector{Int})
     if length(layer_arch)<2
-        error(string("Neural Net must have at least two neuron layers but ",length(layer_arch)," where given.")
+        error(string("Neural Net must have at least two neuron layers but ",length(layer_arch)," where given."))
 	end
-    weights = [randn(Float32, (layer_arch[i], layer_arch[i+1])) for i in 1:(length(weights)-1)]
-    biases = [randn(Float32, (layer_arch[i])) for i in 1:length(weights)]
+    weights = [randn(Float32, (layer_arch[i], layer_arch[i+1])) for i in 1:(length(layer_arch)-1)]
+    biases = [randn(Float32, (layer_arch[i])) for i in 1:length(layer_arch)]
 	return NeuNet(name, layer_arch, weights, biases)
 end
 
-function propagate(net::NeuNet, input::Vector, activation::function)
-    if length(input) != length(net.layer_arch[0])
+function propagate(net::NeuNet, input::Vector, activation::Function)
+    if length(input) != net.layer_arch[1]
         error(string("Input data of size ",length(input)," is incompatible with input layer of size ",length(net.layer_arch[0])))
     end
     previous_layer = input
     for i in 1:length(net.weights)
-        previous_layer = activation.( net.weights[i] * previous_layer + net.biases[i] )
+        previous_layer = activation.( transpose(net.weights[i])*previous_layer + net.biases[i] )
     end
     return previous_layer
 end
 
-function cost(real_output, expected_output)
-
-end
-
 function sigmoid(x::Real)
     return  1.0 / (1.0 + exp(-x))
+end
+
+function cost(real_output, expected_output)
+    
 end
